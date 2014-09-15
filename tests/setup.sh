182142bc116dbdd7b977ff915060ec5b605d3b8f
Jacob Morton <mkoopajr@gmail.com>
2014-09-15 03:07:48 -0500

    Trying travis with script

---
+++ tests/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mongo admin --eval 'db.createUser("admin", "admin");'
+mongo test --eval 'db.createUser("test", "test123");'
+mongo test --eval 'db.createUser("sessionHandler", "supersecretpassword", roles: ["readWrite"]);'
