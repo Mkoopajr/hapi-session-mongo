d87d7d39bc561107771e9872a9f62bc87eb7d512
Jacob Morton <mkoopajr@gmail.com>
2014-09-15 07:16:42 -0500

    Moved stuff around

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
+mongo test --eval 'db.addUser("test", "test123");'
+mongo test --eval 'db.addUser({user: "sessionHandler", pwd: "supersecretpassword", roles: [{role: "readWrite", db: "test"}]});'
 mongo admin --eval 'db.addUser({user: "admin", pwd: "admin", roles: [{role: "readWriteAnyDatabase", db: "admin"}, {role: "userAdminAnyDatabase", db: "admin"}]});'
-mongo -u admin -p admin test --eval 'db.addUser("test", "test123");'
-mongo -u admin -p admin test --eval 'db.addUser({user: "sessionHandler", pwd: "supersecretpassword", roles: [{role: "readWrite", db: "test"}]});'
