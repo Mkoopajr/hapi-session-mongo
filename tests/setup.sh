296681b77e6e542990730910e06c3f1b700f8d94
Jacob Morton <mkoopajr@gmail.com>
2014-09-15 06:43:58 -0500

    Fixing travis errors

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mongo admin --eval 'db.addUser("admin", "admin");'
-mongo test --eval 'db.addUser("test", "test123");'
-mongo test --eval 'db.addUser("sessionHandler", "supersecretpassword", roles: ["readWrite"]);'
+mongo admin --eval 'db.addUser("admin", "admin", "roles": [{role: "readWriteAnyDatabase", db: "admin"}, {role: "userAdminAnyDatabase", db: "admin"}]);'
+mongo -u admin -p admin test --eval 'db.addUser("test", "test123");'
+mongo -u admin -p admin test --eval 'db.addUser("sessionHandler", "supersecretpassword", "roles": [{role: "readWrite", db: "test"}]);'
