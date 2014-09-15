380e82c1b847a2f69998da7ec6214e2358de31c2
Jacob Morton <mkoopajr@gmail.com>
2014-09-15 07:04:23 -0500

    Fixed typos

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mongo --ssl admin --eval 'db.addUser({user: "admin", pwd: "admin", roles: [{role: "readWriteAnyDatabase", db: "admin"}, {role: "userAdminAnyDatabase", db: "admin"}]});'
-mongo --ssl -u admin -p admin test --eval 'db.addUser("test", "test123");'
-mongo --ssl -u admin -p admin test --eval 'db.addUser({user: "sessionHandler", pwd: "supersecretpassword", roles: [{role: "readWrite", db: "test"}]});'
+mongo admin --eval 'db.addUser({user: "admin", pwd: "admin", roles: [{role: "readWriteAnyDatabase", db: "admin"}, {role: "userAdminAnyDatabase", db: "admin"}]});'
+mongo -u admin -p admin test --eval 'db.addUser("test", "test123");'
+mongo -u admin -p admin test --eval 'db.addUser({user: "sessionHandler", pwd: "supersecretpassword", roles: [{role: "readWrite", db: "test"}]});'
