90fe179b14f1e86bcf97cf5df70d6ece39f30dcd
Jacob Morton <mkoopajr@gmail.com>
2014-09-16 01:31:06 -0500

    Trying roles another way

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mongo test --eval 'db.addUser({user: "test", pwd: "test123", roles: []});'
-mongo test --eval 'db.addUser({user: "sessionHandler", pwd: "supersecretpassword", roles: [{role: "readWrite", db: "test"}]});'
-mongo admin --eval 'db.addUser({user: "admin", pwd: "admin", roles: [{role: "readWriteAnyDatabase", db: "admin"}, {role: "userAdminAnyDatabase", db: "admin"}]});'
+mongo test --eval 'db.addUser({user: "sessionHandler", pwd: "supersecretpassword", roles: ["readWrite"]});'
+mongo admin --eval 'db.addUser({user: "admin", pwd: "admin", roles: ["readWriteAnyDatabase", "userAdminAnyDatabase"]});'
