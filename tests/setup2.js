96547d9072e59f1521b3bcb55910131422dc4975
Jacob Morton <mkoopajr@gmail.com>
2014-09-28 00:59:55 -0500

    Testing new travis stuff

--- tests/setup2.js
+++ tests/setup2.js
@@ -11,30 +11,30 @@ var server = new Server('127.0.0.1', 27017, {ssl: true}),
 
 db.open(function(err, db) {
     if (err) {
-        return err;
+        console.log(err);
     }
 
     db.createCollection('users', function(err, collection) {
         if (err) {
             db.close();
-            return err;
+            console.log(err);
         }
 
         bcrypt.hash('test123', 10, function(err, hash) {
             if (err) {
                 db.close();
-                return err;
+                console.log(err);
             }
 
             collection.insert([{_id: 'test@test.com', local: {name: 'test@test.com', pwd: hash}}],
             function(err, data) {
                 if (err) {
                     db.close();
-                    return err;
+                    console.log(err);
                 }
 
                 db.close();
-                return data;
+                console.log(data);
             });
         });
     });
